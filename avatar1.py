4d46279ada88c3d148ca71dc89d6195a1657625e
ffg72 <91719783+ffg72@users.noreply.github.com>
2022-01-11 11:53:41 +0100

    Add files via upload

--- avatar1.py
+++ avatar1.py
@@ -1,63 +1,73 @@
 import pygame
 from pygame.math import Vector2
 
+import core
 from core import getMouseLeftClick
 
 
 class Avatar:
     def __init__(self):
-        self.taille = 8
+        self.taille = 10
         self.couleur = (255, 255, 255)
         self.masse = 5
         self.position = Vector2(400,450)
         self.vitesse = Vector2(0,0)
         self.vitesseMin = Vector2(0,0)
-        self.vitesseMax = Vector2(0,0)
+        self.vitesseMax = Vector2(0.1,0.1)
+        self.k=0.01
+        self.l0=1
 
 
-    def affichage(self,screen):
-        pygame.draw.circle(screen, self.couleur, self.position, self.taille)
+    def affichage(self):
+        core.Draw.circle(self.couleur, self.position, self.taille)
 
-    def move(self, screen):
 
-        dX, dY = pygame.mouse.get_pos()
-        # Find the angle from the center of the screen to the mouse in radians [-Pi, Pi]
-        rotation = math.atan2(dY - float(SCREEN_HEIGHT)/2, dX - float(SCREEN_WIDTH)/2)
-        # Convert radians to degrees [-180, 180]
-        rotation *= 180/math.pi
-        # Normalize to [-1, 1]
-        # First project the point from unit circle to X-axis
-        # Then map resulting interval to [-1, 1]
-        normalized = (90 - math.fabs(rotation))/90
-        vx = self.vitesse*normalized
-        vy = 0
-        if rotation < 0:
-            vy = -self.vitesse + math.fabs(vx)
-        else:
-            vy = self.vitesse - math.fabs(vx)
-        tmpX = self.x + vx
-        tmpY = self.y + vy
-        self.x = tmpX
-        self.y = tmpY
+    '''
+    def deplacer(self, direction, core):
+        if core.getMouseLeftClick().x - self.position.x and core.getMouseLeftClick().y - self.position.y :
+            None
+    '''
 
+    def move(self,destination):
+        if destination is not None:
+            #bilan des forces
+            #F=k * u * |l-l0|
+
+            l=self.position.distance_to(destination)
+            u=destination-self.position
+            u=u.normalize()
+
+            F=self.k*u*abs(l-self.l0)
+            print(F)
+
+            #limiter la force max
+
+
+            #ajouter la force a la vitesse
+            self.vitesse = self.vitesse + F
+
+        #limiter la vitesse
+        if self.vitesse > self.vitesseMax:
+            self.vitesse = self.vitesseMax
+
+
+
+        #ajouter la vitesse a la position
+
+
+        self.position= self.position+self.vitesse
 
     def deplacer(self, direction, core):
-
-        getMouseLeftClick()
-
         if direction > 0 and self.position.x < core.WINDOW_SIZE[0] - self.taille.x:
             self.position.x += self.vitesse
         if direction < 0 and self.position.x > 0:
             self.position.x -= self.vitesse
 
-        if direction > 0 and self.position.y < core.WINDOW_SIZE[0] - self.taille.y:
-            self.position.y += self.vitesse
-        if direction < 0 and self.position.y > 0:
-            self.position.y -= self.vitesse
-
+    def grossir(self,g):
+        pass
 
     def setVitesse(self, v):
-            self.vitesse = v
+        self.vitesse = v
 
     def setPosition(self, pos):
         self.position = pos
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pygame
 from avatar1 import Avatar
 import core
 from creep import Creep
@@ -9,14 +8,11 @@ def setup():
     core.fps = 30
     core.WINDOW_SIZE = [1200, 850]
 
-    #core.memory("centredecercle", [200, 200])
-    #core.memory("rayonducercle", 10)
-    #core.memory("couleurducercle", (255, 0, 0))
 
     print("Setup END-----------")
-
-    core.memory("listeCreep",[])
-    for c in range(0,99):
+    core.memory("listeCreep", [])
+    core.memory("avatar",Avatar())
+    for c in range(99):
         core.memory("listeCreep").append(Creep())
 
 def run():
@@ -24,8 +20,8 @@ def run():
     for c in core.memory("listeCreep"):
         c.affichage(core.screen)
 
-def affichage():
-    Avatar.affichage(core.screen)
+    core.memory("avatar").affichage()
+    core.memory("avatar").move(core.getMouseLeftClick())
 
 
 core.main(setup, run)
